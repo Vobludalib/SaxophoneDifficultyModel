36605c4129da5b10f9ded0ef65091cdb8c2df6e2
librickysimon <librickysimon@gmail.com>
2024-10-17 14:26:44 +0200

    Added trill detection tests + encodings

--- trill_detection_test.py
+++ trill_detection_test.py
@@ -4,15 +4,39 @@ import matplotlib.pyplot as plt
 import sklearn.cluster as skc
 import scipy.signal
 from scipy.signal.windows import gaussian
+import sys
+
+def cleanup_nan(f_0s):
+    prev_val = float('nan')
+    curr = []
+    newlen = 0
+    for val in f_0s:
+        if np.isnan(val):
+            if np.isnan(prev_val):
+                pass
+        
+            else:
+                curr.append(prev_val)
+                newlen += 1
+
+        else:
+            curr.append(val)
+            newlen += 1
+
+    return np.asarray(curr), newlen
+
+f_path = sys.argv[1]
+print(f_path)
 
 print("Loading audio!")
-audio, sample_rate = librosa.load(r"/Users/slibricky/Desktop/Thesis/thesis/testTrill.wav")
+audio, sample_rate = librosa.load(f_path)
+num_of_seconds = librosa.get_duration(y=audio, sr=sample_rate)
 print("Finished loading!")
 f_0, _, _ = librosa.pyin(y=audio, fmin=librosa.note_to_hz("A2"), fmax=librosa.note_to_hz("C5"), frame_length=1024)
 sample_length = 22050/1024
 midi_vals = np.asarray(librosa.hz_to_midi(list(f_0)))
-times = [i*46.5 for i in range (0, len(midi_vals))]
-midi_vals = midi_vals[1:]
+midi_vals, newlen = cleanup_nan(midi_vals)
+times = [i*46.5 for i in range (0, newlen)]
 two_means_cluster = skc.KMeans(n_clusters=2).fit(midi_vals.reshape(-1, 1))
 notes1 = []
 notes2 = []
@@ -51,6 +75,6 @@ for midi_val in midi_vals:
         time_spent_on_note = 0
 
 print(changes)
-
-# plt.plot(times[1:], midi_vals)
-# plt.savefig(r"./trill.png")
+print(num_of_seconds)
+print(f"Average trill speed: {changes/num_of_seconds * 0.5} - one trill is a period")
+print(f"Trilling from {librosa.midi_to_note(note1_midi + 2)} to {librosa.midi_to_note(note2_midi + 2)}")
