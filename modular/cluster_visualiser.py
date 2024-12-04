d2b93cb831faffbe2411696bbf6947f4962919f9
librickysimon <librickysimon@gmail.com>
2024-12-04 15:34:21 +0100

    Cluster visualiser work

--- modular/cluster_visualiser.py
+++ modular/cluster_visualiser.py
@@ -17,22 +17,34 @@ def main():
             else:
                 cluster_dict[cluster].append(speed)
 
-        xs = flat_list = [
+        xs = [
                             speed
-                            for cluster in cluster_dict.values()
-                            for speed in cluster
+                            for key in cluster_dict.keys()
+                            for speed in cluster_dict[key] if key != -1
                         ]
-        ys_prep = [(len(cluster_dict[key])) for key in cluster_dict.keys()]
-        print(ys_prep)
+        logxs = np.log2(xs)
+        ys_prep = [(len(cluster_dict[key]), key) for key in cluster_dict.keys() if key != -1]
         ys = []
         colors = []
-        for label, cluster_len in enumerate(ys_prep):
+        annotations = []
+        log_annotations = []
+        for label, (cluster_len, key) in enumerate(ys_prep):
             cluster_color = (random.uniform(0,1), random.uniform(0, 1), random.uniform(0, 1))
+            max_speed_in_cluster = np.max(cluster_dict[key])
+            annotations.append((label, key, max_speed_in_cluster))
+            log_annotations.append((label, key, np.log2(max_speed_in_cluster)))
             for i in range(cluster_len): 
                 ys.append(label)
                 colors.append(cluster_color)
         plt.scatter(xs, ys, color=colors)
-        plt.show()
+        for (y, cluster_id, max) in annotations:
+            plt.annotate(str(cluster_id), (max, y))
+        plt.savefig('./files/session_clusters_normal.png')
+        plt.clf()
+        plt.scatter(logxs, ys, color=colors)
+        for (y, cluster_id, max) in log_annotations:
+            plt.annotate(str(cluster_id), (max, y))
+        plt.savefig('./files/log_session_clusters_normal.png')
 
 if __name__ == "__main__":
     main()
