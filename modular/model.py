8eb42c75b1432105409eb376e59010aeefdea5b0
librickysimon <librickysimon@gmail.com>
2025-02-15 16:33:43 +0100

    Model fitting and data manipulation functions

--- modular/model.py
+++ modular/model.py
@@ -2,3 +2,53 @@
 # a) Features (choose only best or what?)
 # b) Automatic feature extraction (using what methods?)
 # c) Raw encodings into NN
+
+import sklearn.metrics
+import sklearn.model_selection
+import sklearn
+import sklearn.neural_network
+import sklearn.preprocessing
+import encoding
+import numpy as np
+
+def fit_on_mlp(xs, ys) -> sklearn.neural_network.MLPRegressor:
+    mlp = sklearn.neural_network.MLPRegressor(hidden_layer_sizes=(40,10), max_iter=1500)
+    mlp.fit(xs, ys)
+    return mlp
+
+def transitions_trill_dict_to_numpy_arrays(transitions_to_trill_dict):
+    transitions = transitions_to_trill_dict.keys()
+    trill_speeds_grouped = transitions_to_trill_dict.values()
+    trill_speeds = []
+    for trill_speed in trill_speeds_grouped:
+        trill_speeds.append(np.sum(trill_speed)/len(trill_speed))
+
+    xs, ys = transitions_and_speed_lists_to_numpy_arrays(transitions, trill_speeds)
+
+    return xs, ys
+
+def transitions_and_speed_lists_to_numpy_arrays(transitions, speeds):
+    # For now using this features method -> possibly to be changed for some experiments
+    features = [encoding.generate_transition_features(trans, style='expert', expert_weights=False) for trans in transitions]
+    xs = np.asarray(features)
+    ys = np.asarray(speeds)
+
+    return xs, ys
+
+def main():
+    transitions_to_trill_dict = encoding.load_transitions_from_file('/Users/slibricky/Desktop/Thesis/thesis/modular/files/PlatonSession0BatchAnalysed.csv')
+    xs, ys = transitions_trill_dict_to_numpy_arrays(transitions_to_trill_dict)
+    xs = xs[:,2:]
+
+    train_xs, test_xs, train_ys, test_ys = sklearn.model_selection.train_test_split(xs, ys, test_size=0.1)
+
+    mlp = fit_on_mlp(train_xs, train_ys)
+    predicts = mlp.predict(test_xs)
+    mse = sklearn.metrics.mean_squared_error(test_ys, predicts)
+    print(mse)
+    comparison = np.vstack([test_ys, predicts])
+    print(comparison.T)
+
+
+if __name__ == '__main__':
+    main()
