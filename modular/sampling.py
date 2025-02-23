f62a0d951588475cb13eff44a0b7f3a1cb9a91a1
librickysimon <librickysimon@gmail.com>
2025-02-23 17:43:27 +0100

    Work on sampling tests

--- modular/sampling.py
+++ modular/sampling.py
@@ -1,4 +1,5 @@
 import sklearn.metrics
+import sklearn.model_selection
 import encoding
 import numpy as np
 import sklearn.cluster as skc
@@ -11,52 +12,25 @@ import sklearn
 
 # FROM ALL FINGERINGS SUBSAMPLE USING 
 # a) UNIFORM RANDOMNESS
-# b) FREQUENCY IN WJDB
+# b) EMPIRICAL (OCCURENCE FREQUENCY) IN WJDB
 # c) BASED ON CLUSTERING
 
 # Method for subsampling using uniform randomness
-def uniform_subsample(transitions_trill_speed_dict: dict, n, seed=10, print_debug=False):
+def uniform_subsample(transition_list: list, trill_speeds, n, seed=10, print_debug=False):
     random.seed(seed)
 
     sampled_trans = []
     sampled_speeds = []
-    not_sampled_trans = []
-    not_sampled_speeds = []
 
-    amount_of_transitions = len(transitions_trill_speed_dict)
-    transitions = list(transitions_trill_speed_dict.keys())
+    sampled_indexes = np.array(random.sample([i for i in range(len(transition_list))], n))
+    for index in sampled_indexes:
+        sampled_trans.append(transition_list[index])
+    sampled_speeds = trill_speeds[sampled_indexes]
 
-    indices = [i for i in range(amount_of_transitions)]
-    for i in range(n):
-        raw_index = random.randint(0, amount_of_transitions - i - 1)
-        sampled_index = indices[raw_index]
-        trans = transitions[sampled_index]
-        speeds_for_trans = transitions_trill_speed_dict[trans]
-        amount_of_speeds = len(speeds_for_trans)
-        sampled_speed_index = random.randint(0, amount_of_speeds-1)
-        for index, val in enumerate(speeds_for_trans):
-            if index == sampled_speed_index:
-                sampled_trans.append(trans)
-                sampled_speeds.append(val)
-            else:
-                not_sampled_trans.append(trans)
-                not_sampled_speeds.append(val)
-        
-        del indices[raw_index]
-    
-    for not_selected_index in indices:
-        trans = transitions[not_selected_index]
-        for speed in transitions_trill_speed_dict[trans]:
-            not_sampled_trans.append(trans)
-            not_sampled_speeds.append(speed)
-
-    return sampled_trans, sampled_speeds, not_sampled_trans, not_sampled_speeds
+    return sampled_trans, sampled_speeds
 
 # Method for subsampling using WJDB frequencies
-def occurence_frequency_subsample(transitions, n, seed=10, print_debug=False):
-
-    transitions = list(transitions)
-
+def empirical_subsample(transition_list: list, trill_speeds, n, seed=10, print_debug=False):
     pathToCsv = "/Users/slibricky/Desktop/Thesis/melospy-gui_V_1_4b_mac_osx/bin/analysis/feature+viz/bigramsTS.csv"
 
     bigramDict = { }
@@ -86,122 +60,130 @@ def occurence_frequency_subsample(transitions, n, seed=10, print_debug=False):
 
     midiToOccurencesDict = dict(sorted(bigramDict.items(), key= lambda x: x[1]))
 
-    print(midiToOccurencesDict)
-
-    midiToTransitionsDict = {}
-
-    for transition in transitions:
-        key = (transition[0].midi, transition[1].midi)
-        if midiToTransitionsDict.get(key, None) is None:
-            midiToTransitionsDict[key] = [transition]
-        else:
-            midiToTransitionsDict[key].append(transition)
-
-    transitionsList = []
-    occurencesList = []
-    for transition in transitions:
-        transitionsList.append(transition)
-        key = (transition[0].midi, transition[1].midi)
-        # Smoothing for unrepresented values
-        occurencesList.append(midiToOccurencesDict.get(key, 0.01))
+    occurencesList = [midiToOccurencesDict.get((trans.fingering1.midi, trans.fingering2.midi), 0.1) for trans in transition_list]
 
     probabilities = np.asarray(occurencesList) / np.sum(occurencesList)
     np.random.seed(seed)
-    indexes = np.asarray([i for i in range(len(transitions))])
+    indexes = np.asarray([i for i in range(len(transition_list))])
     selected_indexes = np.random.choice(a=indexes, p=probabilities, size=n, replace=False)
-    selected = []
+    selected_trans = []
+    selected_speeds = []
     for index in selected_indexes:
-        selected.append(transitionsList[index])
-    not_selected = []
-    for transition in transitions:
-        if transition not in selected:
-            not_selected.append(transition)
+        selected_trans.append(transition_list[index])
+        selected_speeds.append(trill_speeds[index])
 
-    return selected, not_selected
+    selected_speeds = np.asarray(selected_speeds)
+    return selected_trans, selected_speeds
 
-# TODO: TEST SET created from leftovers from sampling -> subsample leftovers to get consistent test set size
-# Would mean we can only subsample within some range for testing (85% to 15%)
-# Is it possible to reserve some data as a test set?
-# Cons for me: We don't have a fixed size for each cluster, so we cannot just take one from each cluster or something like that
 # Method for subsampling based on clusters
-def cluster_subsample(transitions, n, seed=10, print_debug=False):
+def cluster_subsample(transition_list: list, trill_speeds, n, seed=10, print_debug=False):
     random.seed(seed)
 
-    encoding_feature_pairs = []
-    for transition in transitions:
-        if print_debug: print(f"Going from {transition[0].name} to {transition[1].name}")
-        features = encoding.generate_transition_features(transition)
-        encoding_feature_pairs.append(([transition[0], transition[1]], features))
-    
     clusters_dict = {}
-    _, labels, _ = skc.k_means(n_clusters=n, X=np.asarray([pair[1] for pair in encoding_feature_pairs]))
-    for label in range(n):
-        if print_debug: print(f"=== PROCESSING LABEL {label} ===")
-        for index, elem in enumerate(encoding_feature_pairs):
-            if labels[index] == label:
-                if print_debug: print(f"{elem[0]} with feat {elem[1]}")
-                if label not in clusters_dict:
-                    clusters_dict[label] = [elem[0]]
-                else:
-                    clusters_dict[label].append(elem[0])
+    transition_features = [encoding.generate_transition_features(trans, style='expert', expert_weights=False) for trans in transition_list]
+    _, labels, _ = skc.k_means(n_clusters=n, X=np.asarray(transition_features))
+    for index, transition in enumerate(transition_list):
+        label = labels[index]
+        if clusters_dict.get(label, None) is None:
+            clusters_dict[label] = [(transition, trill_speeds[index])]
+        else:
+            clusters_dict[label].append((transition, trill_speeds[index]))
 
-    if print_debug: print(f"In total there are {len(encoding_feature_pairs)} unique trills using default fingerings on TS")
+    selected_trans = []
+    selected_speeds = []
+    for cluster_label in clusters_dict:
+        sample = random.sample(clusters_dict[cluster_label], 1)[0]
+        trans = sample[0]
+        speed = sample[1]
+        selected_trans.append(trans)
+        selected_speeds.append(speed)
 
-    selected_transitions = []
-    not_selected_transition = []
-    for cluster in clusters_dict:
-        selected_transitions.append(random.sample(clusters_dict[cluster], 1)[0])
-        for transition in clusters_dict[cluster]:
-            if transition is not selected_transitions[-1]:
-                not_selected_transition.append(transition)
+    selected_speeds = np.asarray(selected_speeds)
+    return selected_trans, selected_speeds
 
-    return selected_transitions, not_selected_transition
+def get_stratified_kfold(xs, ys, test_size, seed=10):
+    amount_of_data_points = len(xs)
+    amount_of_folds = amount_of_data_points//test_size
+    bins = np.array([0, 1.5, 3, 4.5, 10])
+    binned_ys = np.digitize(ys, bins)
+    skf = sklearn.model_selection.StratifiedKFold(amount_of_folds)
+    folds = []
+    for i, (train_index, test_index) in enumerate(skf.split(xs, binned_ys)):
+        folds.append((i, train_index, test_index))
 
-def bootstrap_sample(xs, ys, n, seed=10):
-    random.seed(seed)
-    indices = [random.randrange(0, len(xs)) for i in range(n)]
-    sampled_xs = []
-    sampled_ys = []
-    for index in indices:
-        sampled_xs.append(xs[index])
-        sampled_ys.append(ys[index])
-    
-    return sampled_xs, sampled_ys
+    return folds
 
-def perform_sampling_test(transitions_trill_speed_dict, sampling_method, size_of_test_set = 15):
+def perform_sampling_test(transitions_trill_speed_dict, sampling_method, size_of_test_set = 20, minimum_amount_of_samples=10):
+    # For the sake of the sampling test, for each transition we uniformly randomly select only one of its recorded intervals
+    xs = []
+    ys = []
+
+    sampling_func = None
+    if sampling_method == 'uniform':
+        sampling_func = uniform_subsample
+    elif sampling_method == 'cluster':
+        sampling_func = cluster_subsample
+    elif sampling_method == 'empirical':
+        sampling_func = empirical_subsample
+    else:
+        raise NotImplementedError
+
+    for key in transitions_trill_speed_dict:
+        if len(transitions_trill_speed_dict[key]) == 1:
+            transitions_trill_speed_dict[key] = transitions_trill_speed_dict[key][0]
+        else:
+            transitions_trill_speed_dict[key] = random.sample(transitions_trill_speed_dict[key], 1)[0]
+        
+        xs.append(key)
+        ys.append(transitions_trill_speed_dict[key])
+
+    ys = np.asarray(ys)
+
     errors = []
-    number_of_retries_per_i = 10
-    minimum_amount_of_elements_in_train_set = 20
-    amount_of_fingerings_recorded = len(transitions_trill_speed_dict)
-    for i in tqdm(range(amount_of_fingerings_recorded - size_of_test_set - minimum_amount_of_elements_in_train_set)):
-        seed_i = random.randint(0, 100000)
-        amount_to_sample = i + minimum_amount_of_elements_in_train_set
-        amount_leftover = amount_of_fingerings_recorded - amount_to_sample
-        mse_sum = 0
-        for j in range(number_of_retries_per_i):
-            selected_xs, selected_ys, not_selected_xs, not_selected_ys = uniform_subsample(transitions_trill_speed_dict, amount_to_sample, seed=seed_i)
-            bootstrap_xs, boostrap_ys = bootstrap_sample(not_selected_xs, not_selected_ys, size_of_test_set, seed=seed_i+j)
-            selected_xs, selected_ys = model.transitions_and_speed_lists_to_numpy_arrays(selected_xs, selected_ys)
-            bootstrap_xs, boostrap_ys = model.transitions_and_speed_lists_to_numpy_arrays(bootstrap_xs, boostrap_ys)
-            # TRAIN MODEL ON SELECTED CLUSTER
-            mlp = model.fit_on_mlp(selected_xs, selected_ys)
-            predicts = mlp.predict(bootstrap_xs)
-            mse = sklearn.metrics.mean_squared_error(boostrap_ys, predicts)
-            mse_sum += mse
-            
-        errors.append((i, mse_sum/number_of_retries_per_i))
+
+    folds = get_stratified_kfold(xs, ys, test_size=size_of_test_set)
+    for i, train_index, test_index in folds:
+        errors.append([])
+        print(f"=== Doing fold {i} ===")
+        train_xs = []
+        train_ys = []
+        test_xs = []
+        test_ys = []
+        for train_i in train_index:
+            train_xs.append(xs[train_i])
+        train_ys = ys[train_index]
+        for test_i in test_index:
+            test_xs.append(xs[test_i])
+        test_ys = ys[test_index]
+
+        # Iterate over every n possible to subsample
+        for j in range(train_ys.shape[0] - minimum_amount_of_samples):
+            print(f"Sampling only {j + minimum_amount_of_samples} samples out of {train_ys.shape[0]}")
+            amount_to_sample = j + minimum_amount_of_samples
+            selected_trans, selected_ys = sampling_func(train_xs, train_ys, n=amount_to_sample)
+            train_features, train_selected_ys = model.transitions_and_speed_lists_to_numpy_arrays(selected_trans, selected_ys)
+            mlp = model.fit_on_lm(train_features, train_selected_ys)
+            test_features, test_ys = model.transitions_and_speed_lists_to_numpy_arrays(test_xs, test_ys)
+            predicts = mlp.predict(test_features)
+            error = sklearn.metrics.mean_squared_error(test_ys, predicts)
+            errors[i].append(error)
 
     return errors
 
 def main():
     # fingerings = encoding.load_fingerings_from_file("/Users/slibricky/Desktop/Thesis/thesis/modular/documentation/encodings.txt")
-    transitions_speed_dict = encoding.load_transitions_from_file("/Users/slibricky/Desktop/Thesis/thesis/modular/files/PlatonSession0BatchAnalysed.csv")
-    errors = perform_sampling_test(transitions_speed_dict, sampling_method='uniform')
+    transitions_speed_dict = encoding.load_transitions_from_file("/Users/slibricky/Desktop/Thesis/thesis/modular/data_with_pilot.csv")
+    # TODO: Filter out same-note trills -> huge outliers
+    min_samples = 20
+    errors = perform_sampling_test(transitions_speed_dict, sampling_method='cluster', minimum_amount_of_samples=min_samples)
     
-    unzipped = list(zip(*errors))
-    xs = unzipped[0]
-    ys = unzipped[1]
-    plt.plot(xs, ys)
+    for fold_index in range(len(errors)):
+        xs = np.array([i + min_samples for i in range(len(errors[fold_index]))])
+        ys = np.array(errors[fold_index])
+        plt.subplot(3, 3, fold_index + 1)
+        plt.plot(xs, ys, label = f"Fold {fold_index}")
+        plt.legend()
+
     plt.show()
 
 
