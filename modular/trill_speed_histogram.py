3e295eecb9aee7f1f501e168fd51ac48f1c312c1
librickysimon <librickysimon@gmail.com>
2025-02-23 17:42:55 +0100

    Script for histogram of trill speeds

---
+++ modular/trill_speed_histogram.py
@@ -0,0 +1,10 @@
+import encoding
+import matplotlib.pyplot as plt
+import numpy as np
+
+trans_to_trill_dict = encoding.load_transitions_from_file('./data.csv')
+xss = list(trans_to_trill_dict.values())
+speeds = np.asarray([ x for xs in xss for x in xs]).T
+
+plt.hist(speeds, bins=20)
+plt.show()
