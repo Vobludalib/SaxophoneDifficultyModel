379e051cbe358bb02473d60b41ab6eecbf2778b1
librickysimon <librickysimon@gmail.com>
2025-03-14 10:04:59 +0100

    Fixed repeated note fingering prediction

--- modular/fingering_prediction.py
+++ modular/fingering_prediction.py
@@ -8,11 +8,26 @@ import numpy as np
 # TODO: Mess around with different distance functions
 # TODO: Handle same-note transitions
 # TODO: Refactor into a class that does this and works over the FingeringTransitionModel class
-def midi_to_fingering_prediction(midi_values: list[int], difficulty_model, midi_to_fingering_dict: dict, feature_extractor):
-    for midi_note in midi_values:
+def midi_to_fingering_prediction(midi_values: list[int], difficulty_model: model.FingeringTransitionModel, midi_to_fingering_dict: dict, feature_extractor):
+    prev_midi = -1
+    guide_index = -1
+    deduplicated_midi_values = []
+    duplication_guide = []
+    for i, midi_note in enumerate(midi_values):
         key = midi_to_fingering_dict.get(midi_note, None)
         if key is None:
             raise Exception(f"Midi value {midi_note} has no fingerings")
+        
+        # Preprocess repeated notes of the same value by removing duplicates, and then adding again at the end
+        if prev_midi != midi_note:
+            guide_index += 1
+            deduplicated_midi_values.append(midi_note)
+            prev_midi = midi_note
+            duplication_guide.append(guide_index)
+        else:
+            duplication_guide.append(guide_index)
+
+    midi_values = deduplicated_midi_values
         
     # Memory is in the form [fingering, shortest distance from start to this midi value played by this note, [list of fingerings on the shortest path]]
     memory = [[fingering, 0, []] for fingering in midi_to_fingering_dict[midi_values[0]]]
@@ -37,7 +52,12 @@ def midi_to_fingering_prediction(midi_values: list[int], difficulty_model, midi_
 
             best_path.append(memory[best_i][0])
 
-            return best_distance, best_path 
+            # Perform duplication to counter the initial deduplication
+            reduplicated_best_path = []
+            for index in duplication_guide:
+                reduplicated_best_path.append(best_path[index])
+
+            return best_distance, reduplicated_best_path 
         
         current_viable_fingerings = [memory_unit[0] for memory_unit in memory]
         fingerings2 = midi_to_fingering_dict[pair[1]]
@@ -45,14 +65,7 @@ def midi_to_fingering_prediction(midi_values: list[int], difficulty_model, midi_
         new_memory = []
         for fing1_index, fing_1 in enumerate(current_viable_fingerings):
             for fing2_index, fing_2 in enumerate(fingerings2):
-                predicted_trill_speed = model.predict_fingering_transition(difficulty_model, fing_1, fing_2, feature_extractor=feature_extractor)
-                if pair[0] == pair[1]:
-                    # Handling same-note transitions as being 'forced' to stay on one fingering to prevent
-                    # changing same-note fingerings during repeated appearances
-                    if fing_1 != fing_2:
-                        predicted_trill_speed = 0.00001
-                    else:
-                        predicted_trill_speed = 10
+                predicted_trill_speed = difficulty_model.predict_transitions([encoding.Transition(fing_1, fing_2)])[0]
                 if fing1_index == 0:
                     new_memory.append([fing_2, memory[fing1_index][1] + predicted_trill_speed, memory[fing1_index][2].copy()])
                     new_memory[fing2_index][2].append(fing_1)
