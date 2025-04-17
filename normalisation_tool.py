17b426a9ba32e66f59fc71ab89f407ce8b09c45e
librickysimon <librickysimon@gmail.com>
2025-04-17 18:06:44 +0200

    Normalisation tool made OOP and multiplicative added

--- normalisation_tool.py
+++ normalisation_tool.py
@@ -5,94 +5,155 @@ import argparse
 import os
 import numpy as np
 import scipy.special
+import abc
+import scipy.stats.mstats
 
 # Takes seperate batch_analysed csvs (each corresponding to one session) and generates one big CSV of trills
-# normalised using the anchor intervals
+# normalised using the anchor transitions
 
 # ANCHORS ARE TAKEN AS BEING CLUSTER -1
-
-def load_anchors_from_file(file_path) -> list:
-    output = []
-    with open(file_path, 'r') as csvf:
-        reader = csv.reader(csvf)
-        next(reader, None)
-        for row in reader:
-            cluster = int(row[1])
-            if (cluster == -1):
-                midi1 = int(row[2])
-                name1 = row[3]
-                encoding1 = row[4]
-                midi2 = int(row[5])
-                name2 = row[6]
-                encoding2 = row[7]
-                speed = float(row[8])
-                output.append((Fingering(midi1, name1, encoding1), Fingering(midi2, name2, encoding2), speed))
-
-    return output
-
-def load_anchors_from_directory(directory_path) -> dict:
-    file_to_anchors_dict = {}
-    for file in os.listdir(directory_path):
-        filename = os.fsdecode(file)
-        if filename.endswith('.csv'):
-            full_path = os.path.join(directory_path, filename)
-            file_to_anchors_dict[filename] = load_anchors_from_file(full_path)
-
-    return file_to_anchors_dict
 
 def get_euclidean_distance(features1, features2):
         return np.sum([((features1[i] - features2[i]) ** 2) ** 0.5 for i in range(features1.shape[0])])
 
-def calculate_anchor_speeds(file_to_anchors_dict) -> tuple[list, list]:
-    """
-    Return order is: list of transition objects, speeds.
-    Speeds is a 2d list, row = session, column = anchor transition
-    """
+class NormalisationTool(abc.ABC):
+    pass
 
-    anchor_intervals_sorted = []
-    checked = False
-    for filename in file_to_anchors_dict:
-        file_to_anchors_dict[filename].sort(key = lambda tup: (tup[0].midi, tup[1].midi))
-        if not checked:
-            for tup in file_to_anchors_dict[filename]:
-                anchor_intervals_sorted.append((tup[0], tup[1]))
-            checked = True
+class AnchorBasedNormaliser(NormalisationTool):
+    @staticmethod
+    def load_anchors_from_file(file_path) -> list:
+        output = []
+        with open(file_path, 'r') as csvf:
+            reader = csv.reader(csvf)
+            next(reader, None)
+            for row in reader:
+                cluster = int(row[1])
+                if (cluster == -1):
+                    midi1 = int(row[2])
+                    name1 = row[3]
+                    encoding1 = row[4]
+                    midi2 = int(row[5])
+                    name2 = row[6]
+                    encoding2 = row[7]
+                    speed = float(row[8])
+                    output.append((Fingering(midi1, name1, encoding1), Fingering(midi2, name2, encoding2), speed))
 
-    speeds = np.zeros((len(file_to_anchors_dict), len(anchor_intervals_sorted)), dtype=float)
-    for row, filename in enumerate(file_to_anchors_dict):
-        for column, interval_tup in enumerate(file_to_anchors_dict[filename]):
-            speeds[row, column] = interval_tup[2]
+        return output
 
-    return anchor_intervals_sorted, speeds
+    @staticmethod
+    def load_anchors_from_directory(directory_path) -> dict:
+        file_to_anchors_dict = {}
+        for file in os.listdir(directory_path):
+            filename = os.fsdecode(file)
+            if filename.endswith('.csv'):
+                full_path = os.path.join(directory_path, filename)
+                file_to_anchors_dict[filename] = AnchorBasedNormaliser.load_anchors_from_file(full_path)
 
-def calculate_difference_to_mean(speeds, means):
-    differences = np.zeros((speeds.shape[0], speeds.shape[1]), dtype=float)
-    for row in range(speeds.shape[0]):
-        for column in range(speeds.shape[1]):
-            differences[row, column] = means[column] - speeds[row, column]
+        return file_to_anchors_dict
 
-    return differences
+    @staticmethod
+    def calculate_anchor_speeds(file_to_anchors_dict) -> tuple[list, list]:
+        """
+        Return order is: list of transition objects, speeds.
+        Speeds is a 2d list, row = session, column = anchor transition
+        """
 
-def get_anchor_interval_features(anchor_intervals, feature_extractor: encoding.TransitionFeatureExtractor):
-    anchor_features = []
-    for transition in anchor_intervals:
-        anchor_features.append(feature_extractor.get_features(transition))
-    anchor_features = np.asarray(anchor_features)
-    return anchor_features
+        anchor_transitions_sorted = []
+        checked = False
+        for filename in file_to_anchors_dict:
+            file_to_anchors_dict[filename].sort(key = lambda tup: (tup[0].midi, tup[1].midi))
+            if not checked:
+                for tup in file_to_anchors_dict[filename]:
+                    anchor_transitions_sorted.append((tup[0], tup[1]))
+                checked = True
 
-def normalise_transition(transition: encoding.Transition, transition_speed, session_index, anchor_features, differences, feature_extractor: encoding.TransitionFeatureExtractor, norm_strength=0.2):
-    interval_features = feature_extractor.get_features(transition)
-    distances_to_anchors = np.asarray([get_euclidean_distance(interval_features, anchor_features[j]) for j in range(anchor_features.shape[0])])
-    multiples = np.asarray(scipy.special.softmax(distances_to_anchors))
-    overall_adjustment = np.sum(np.multiply(multiples, differences[session_index])) * norm_strength
-    normalised_speed = transition_speed + overall_adjustment
-    return normalised_speed
+        speeds = np.zeros((len(file_to_anchors_dict), len(anchor_transitions_sorted)), dtype=float)
+        for row, filename in enumerate(file_to_anchors_dict):
+            for column, transition_tup in enumerate(file_to_anchors_dict[filename]):
+                speeds[row, column] = transition_tup[2]
 
-def inverse_normalise_transition(transition_features, normalised_speed, session_index, anchor_features, differences, norm_strength=0.2):
-    distances_to_anchors = np.asarray([get_euclidean_distance(transition_features, anchor_features[j]) for j in range(anchor_features.shape[0])])
-    multiples = np.asarray(scipy.special.softmax(distances_to_anchors))
-    overall_adjustment = np.sum(np.multiply(multiples, differences[session_index])) * norm_strength
-    return normalised_speed - overall_adjustment
+        return anchor_transitions_sorted, speeds
+
+    @staticmethod
+    def get_anchor_transition_features(anchor_transitions, feature_extractor: encoding.TransitionFeatureExtractor):
+        anchor_features = []
+        for transition in anchor_transitions:
+            anchor_features.append(feature_extractor.get_features(transition))
+        anchor_features = np.asarray(anchor_features)
+        return anchor_features
+
+class AdditiveAnchorBasedNormaliser(AnchorBasedNormaliser):
+    def __init__(self, data_path, norm_strength, feature_extractor: encoding.TransitionFeatureExtractor):
+        self.norm_strength = norm_strength
+        self.feature_extractor = feature_extractor
+        self.file_to_anchors_dict = self.load_anchors_from_directory(data_path)
+
+        self.anchor_transitions_sorted, self.speeds = self.calculate_anchor_speeds(self.file_to_anchors_dict)
+        self.averages = np.mean(self.speeds, axis=0)
+
+        self.differences = self.calculate_difference_to_mean(self.speeds, self.averages)
+
+        self.anchor_features = self.get_anchor_transition_features(self.anchor_transitions_sorted, self.feature_extractor)
+
+    @staticmethod
+    def calculate_difference_to_mean(speeds, means):
+        differences = np.zeros((speeds.shape[0], speeds.shape[1]), dtype=float)
+        for row in range(speeds.shape[0]):
+            for column in range(speeds.shape[1]):
+                differences[row, column] = means[column] - speeds[row, column]
+
+        return differences
+
+    def get_normalisation_adjustment(self, transition, session_index):
+        transition_features = self.feature_extractor.get_features(transition)
+        distances_to_anchors = np.asarray([get_euclidean_distance(transition_features, self.anchor_features[j]) for j in range(self.anchor_features.shape[0])])
+        multiples = np.asarray(scipy.special.softmax(distances_to_anchors))
+        overall_adjustment = np.sum(np.multiply(multiples, self.differences[session_index])) * self.norm_strength
+        return overall_adjustment
+
+    def normalise(self, transition, trill_speed, session_index):
+        normalised_speed = trill_speed + self.get_normalisation_adjustment(transition, session_index)
+        return normalised_speed
+
+    def inverse_normalise(self, transition, normalised_trill_speed, session_index):
+        return normalised_trill_speed - self.get_normalisation_adjustment(transition, session_index)
+        
+class MultiplicativeAnchorBasedNormaliser(AnchorBasedNormaliser):
+    def __init__(self, data_path, norm_strength, feature_extractor: encoding.TransitionFeatureExtractor):
+        self.norm_strength = norm_strength
+        self.feature_extractor = feature_extractor
+        self.file_to_anchors_dict = self.load_anchors_from_directory(data_path)
+
+        self.anchor_transitions_sorted, self.speeds = self.calculate_anchor_speeds(self.file_to_anchors_dict)
+        self.means = np.mean(self.speeds, axis=0)
+
+        self.ratios = self.calculate_ratios_with_means(self.speeds, self.means)
+
+        self.anchor_features = self.get_anchor_transition_features(self.anchor_transitions_sorted, self.feature_extractor)
+
+    @staticmethod
+    def calculate_ratios_with_means(speeds, means):
+        ratios = np.zeros((speeds.shape[0], speeds.shape[1]), dtype=float)
+        for row in range(speeds.shape[0]):
+            for column in range(speeds.shape[1]):
+                ratios[row, column] = means[column] / speeds[row, column]
+
+        return ratios
+
+    def get_normalisation_coefficient(self, transition, session_index):
+        transition_features = self.feature_extractor.get_features(transition)
+        distances_to_anchors = np.asarray([get_euclidean_distance(transition_features, self.anchor_features[j]) for j in range(self.anchor_features.shape[0])])
+        multiples = np.asarray(scipy.special.softmax(distances_to_anchors))
+        overall_coefficient = scipy.stats.mstats.gmean(self.ratios[session_index], axis=0, weights=multiples)
+        return overall_coefficient
+
+    def normalise(self, transition, trill_speed, session_index):
+        normalised_speed = (1-self.norm_strength) * trill_speed + self.norm_strength * (trill_speed * self.get_normalisation_coefficient(transition, session_index))
+        return normalised_speed
+
+    def inverse_normalise(self, transition, normalised_trill_speed, session_index):
+        denormalised_speed = normalised_trill_speed / (1 - self.norm_strength * (1 + self.get_normalisation_coefficient(transition, session_index)))
+        return denormalised_speed
 
 def main():
     parser = argparse.ArgumentParser(
@@ -101,6 +162,7 @@ def main():
     parser.add_argument('--csvs', type=str, help="Path to the folder of batch-analysed .csv files")
     parser.add_argument('-o', type=str, help="Will store all the outputs into one csv file with this name")
     parser.add_argument("--strength", type=float, help="Specify the normalisation strength")
+    parser.add_argument("--norm_style", type=str, choices=['additive', 'multiplicative'], default='additive')
     args = parser.parse_args()
     if (not os.path.isdir(args.csvs)):
         print("--csvs flag must be set to a directory containing .csv files!")
@@ -108,30 +170,16 @@ def main():
         exit()
 
     # Read all the anchors from every csv into dict {filename: [(encoding1, encoding2, speed)]}
-    
-    file_to_anchors_dict = load_anchors_from_directory(args.csvs)
-
-    anchor_intervals_sorted, speeds = calculate_anchor_speeds(file_to_anchors_dict)
-    print(f"Here is to order in which orders appear in the following matrices (columns):")
-    print(anchor_intervals_sorted)
-    print(f"Full array of anchor interval speeds (row = session, column = anchor):")
-    print(speeds)
-    print(f"Here are their averages over all sessions:")
-    averages = np.mean(speeds, axis=0)
-    print(averages)
-
-    differences = calculate_difference_to_mean(speeds, averages)
-
-    print(f"Here are the differences from the mean (negative means that speed was higher than mean):")
-    print(differences)
-
-    print(f"Here are the features of the anchor intervals:")
-    feature_extractor = encoding.ExpertFeatureNumberOfFingersExtractor()
-    anchor_features = get_anchor_interval_features(anchor_intervals_sorted, feature_extractor)
-    print(anchor_features)
+    normaliser = None
+    if args.norm_style == 'additive':
+        normaliser = AdditiveAnchorBasedNormaliser(args.csvs, args.strength, encoding.ExpertFeatureNumberOfFingersExtractor())
+    elif args.norm_style == 'multiplicative':
+        normaliser = MultiplicativeAnchorBasedNormaliser(args.csvs, args.strength, encoding.ExpertFeatureNumberOfFingersExtractor())
+    else:
+        raise NotImplementedError()
 
     new_csv = []
-    for i, filename in enumerate(file_to_anchors_dict):
+    for i, filename in enumerate(normaliser.file_to_anchors_dict):
         full_path = os.path.join(args.csvs, filename)
         with open(full_path, 'r') as csvf:
             reader = csv.reader(csvf)
@@ -149,13 +197,13 @@ def main():
                 fingering1 = Fingering(midi1, name1, encoding1)
                 fingering2 = Fingering(midi2, name2, encoding2)
                 transition = encoding.Transition(fingering1, fingering2)
-                normalised_speed = normalise_transition(transition, speed, i, anchor_features, differences, feature_extractor, args.strength)
+                normalised_speed = normaliser.normalise(transition, speed, i)
                 # print(f"{filename} - {name1}, {name2}\nOG: {speed}, adjustment: {overall_adjustment}, new: {normalised_speed}")
                 new_csv.append([filename, cluster, midi1, name1, encoding1, midi2, name2, encoding2, normalised_speed])
     
     with open(args.o, 'w') as csvf:
         writer = csv.writer(csvf)
-        writer.writerow(["Filename", "Cluster", "Midi 1 (transposed as written for TS)", "Fingering 1 Name", "Fingering 1 Encoding", "Midi 2 (transposed as written for TS)", "Fingering 2 Name", "Fingering 2 Encoding", "(Normalised) Trill Speed"])
+        writer.writerow(["Filename", "Cluster", "Midi 1 (transposed as written for TS)", "Fingering 1 Name", "Fingering 1 Encoding", "Midi 2 (transposed as written for TS)", "Fingering 2 Name", "Fingering 2 Encoding", "(Normalised) Trill Speed", f"Strength {args.strength}"])
         for row in new_csv:
             writer.writerow(row)
 
