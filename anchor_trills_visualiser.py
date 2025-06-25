520aee20d15fa6d193414eeea2ff0c26d847196e
librickysimon <librickysimon@gmail.com>
2025-06-25 09:39:52 +0200

    Added MAPE experiment for anchor trans

--- anchor_trills_visualiser.py
+++ anchor_trills_visualiser.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 import os
 import argparse
+import statistics
+import sklearn
+import sklearn.metrics
 
 anchors_file = os.path.join(".", "encodings", "anchor_transitions.txt")
 anchor_encoding_pairs = []
@@ -40,7 +43,7 @@ with open(args.data, 'r') as f:
 vals = {}
 player_colors = []
 player_order = []
-player_colors = ['#377eb8', '#ff7f00', '#4daf4a', '#f781bf', '#ffcc33']
+player_colors = ['#ffcc33', '#ff7f00', '#4daf4a', '#f781bf', '#377eb8']
 
 for player in player_to_anchor_trills.keys():
     sessions = player_to_anchor_trills[player].keys()
@@ -71,15 +74,29 @@ for x in vals.keys():
 
 player_order = [f"Player {i+1}" for i in range(len(player_order))]
 
+mape_errors = []
+for trans_val in vals.keys():
+    speeds = []
+    for speed, _ in vals[trans_val]:
+        speeds.append(speed)
+    median = statistics.median(speeds)
+    y_trues = [median for i in range(len(speeds))]
+    mape_error = sklearn.metrics.mean_absolute_percentage_error(y_trues, speeds)
+    mape_errors.append(mape_error)
+    print(f"Trans {trans_val}: {mape_error}")
+    print(anchor_encoding_pairs[trans_val])
+
+print(f"Avg MAPE: {sum(mape_errors)/len(mape_errors)}, Min: {min(mape_errors)}, Max: {max(mape_errors)}")
+
 ax = plt.gca()
 plt.scatter(xs, ys, color=colors)
 plt.xticks(ticks=[0, 1, 2, 3, 4, 5], labels=x_names)
 plt.xlim((0, 6))
 for tick in ax.xaxis.get_majorticklabels():
     tick.set_horizontalalignment("left")
-plt.xlabel("Individual repeated transitions")
+plt.xlabel("Individual anchor transitions")
 plt.ylabel("Trill speed in trills/s")
-plt.title("Visualisation of trill speed variance of repeated transitions")
+plt.title("Visualisation of trill speed variance of anchor transitions")
 plt.grid(visible=True, axis='x')
 legend_patches = [Patch(color=color, label=name) for color, name in zip(player_colors, player_order)]
 plt.legend(handles=legend_patches, loc="upper center")
